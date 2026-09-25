dcbcac7dde7aa4e2d31dfc8cbf5cbc530dbff073
RAGHAV <raghavoppari7@gmail.com>
2026-09-25 22:52:50 +0530

    Fix deployed frontend CORS

--- server/server.js
+++ server/server.js
@@ -22,7 +22,10 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://client-ashen-delta-97.vercel.app",
+    ],
   })
 );
 
